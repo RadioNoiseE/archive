1f9086ddd30bef9842630639c1daa53b2cc42ee7
RadioNoiseE <j18516785606@icloud.com>
2025-03-13 11:00:32 +0800

    syntax highlight fnly

---
+++ script/listing.js
@@ -0,0 +1,10 @@
+(function () {
+    var core = document.createElement('script');
+    core.src = 'https://cdn.jsdelivr.net/npm/prismjs@1.30.0/prism.min.js';
+    core.async = true;
+    document.head.appendChild(core);
+    var autoload = document.createElement('script');
+    autoload.src = 'https://cdn.jsdelivr.net/npm/prismjs@1.30.0/plugins/autoloader/prism-autoloader.min.js';
+    autoload.async = true;
+    document.head.appendChild(autoload);
+})();
