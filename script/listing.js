d0789c5adf75017af98190307c15add13f34a574
RadioNoiseE <j18516785606@icloud.com>
2025-03-13 11:31:16 +0800

    no defer

--- script/listing.js
+++ script/listing.js
@@ -1,10 +1,8 @@
 (function () {
     var core = document.createElement('script');
     core.src = 'https://cdn.jsdelivr.net/npm/prismjs@1.30.0/prism.min.js';
-    core.async = true;
     document.head.appendChild(core);
     var autoload = document.createElement('script');
     autoload.src = 'https://cdn.jsdelivr.net/npm/prismjs@1.30.0/plugins/autoloader/prism-autoloader.min.js';
-    autoload.async = true;
     document.head.appendChild(autoload);
 })();
