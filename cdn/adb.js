1f484d166d294633000a2bcb811e0760a41d38e0
RadioNoiseE <j18516785606@icloud.com>
2025-09-12 22:32:59 +0800

    surge script rewrite

--- cdn/adb.js
+++ cdn/adb.js
@@ -1,22 +1,24 @@
-if ($response?.body && /^https?:\/\/app\.bilibili\.com\/x\/v2\/splash\/list/.test($request.url)) {
-    try {
-        let obj = JSON.parse($response.body);
-        obj.data = {
-            ...obj.data,
-            max_time: 0,
-            min_interval: 31536000,
-            pull_interval: 31536000,
-            list: obj.data.list?.map(item => ({
-                ...item,
-                duration: 0,
-                begin_time: 1915027200,
-                end_time: 1924272000,
-                show: []
-            })) || []
-        };
-        $done({ body: JSON.stringify(obj) });
-    } catch (err) {
-        console.log("Splash removal error:", err);
-        $done();
-    }
-} else { $done(); }
+(() => {
+    if ($response?.body && /^https?:\/\/app\.bilibili\.com\/x\/v2\/splash\/list/.test($request.url)) {
+        try {
+            let object = JSON.parse($response.body);
+            object.data = {
+                ...object.data,
+                max_time: 0,
+                min_interval: 31536000,
+                pull_interval: 31536000,
+                list: object.data.list?.map(item => ({
+                    ...item,
+                    duration: 0,
+                    begin_time: 1915027200,
+                    end_time: 1924272000,
+                    show: []
+                })) || []
+            };
+            $done({ body: JSON.stringify(object) });
+        } catch (error) {
+            console.log("Splash removal error:", error);
+            $done();
+        }
+    } else { $done(); }
+})();
